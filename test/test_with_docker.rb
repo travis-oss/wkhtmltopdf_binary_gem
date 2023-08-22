d004b9dc06ffe04be0f5cee8782a7837d65f9696
vitalie <vitalie@travis-ci.org>
2023-08-22 03:00:33 +0300

    [Test] Code reformat

--- test/test_with_docker.rb
+++ test/test_with_docker.rb
@@ -6,11 +6,11 @@ end
 
 class WithDockerTest < Minitest::Test
   SETUP = begin
-    `docker-compose build --no-cache` unless macos?
-  end
+            `docker-compose build --no-cache` unless macos?
+          end
 
   def test_centos_6
-   test with: 'centos_6'
+    test with: 'centos_6'
   end
 
   def test_centos_7
@@ -18,43 +18,43 @@ class WithDockerTest < Minitest::Test
   end
 
   def test_centos_8
-   test with: 'centos_8'
+    test with: 'centos_8'
   end
 
   def test_debian_9
-   test with: 'debian_9'
+    test with: 'debian_9'
   end
 
   def test_debian_10
-   test with: 'debian_10'
+    test with: 'debian_10'
   end
 
   def test_with_ubuntu_16
-   test with: 'ubuntu_16.04'
+    test with: 'ubuntu_16.04'
   end
 
   def test_with_ubuntu_18
-   test with: 'ubuntu_18.04'
+    test with: 'ubuntu_18.04'
   end
 
   def test_with_ubuntu_20
-   test with: 'ubuntu_20.04'
+    test with: 'ubuntu_20.04'
   end
 
   def test_with_ubuntu_22
-   test with: 'ubuntu_22.04'
+    test with: 'ubuntu_22.04'
   end
 
   def test_with_archlinux
-   test with: 'archlinux'
+    test with: 'archlinux'
   end
-  
+
   def test_rockylinux_8
-   test with: 'rockylinux_8'
+    test with: 'rockylinux_8'
   end
-  
+
   def test_with_macos
-   assert_equal(`bin/wkhtmltopdf --version`.strip, 'wkhtmltopdf 0.12.6 (with patched qt)') if macos?
+    assert_equal(`bin/wkhtmltopdf --version`.strip, 'wkhtmltopdf 0.12.6 (with patched qt)') if macos?
   end
 
   private
